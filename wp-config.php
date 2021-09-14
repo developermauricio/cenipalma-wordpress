0ed6d203c7da1af8d935b5e589913aa3113ba4b4
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2021-09-14 20:37:00 +0000

    first commit

---
+++ wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'cenipalma' );
+
+/** MySQL database username */
+define( 'DB_USER', 'forge' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'HbK3CJVIhe83urtDQ7x2' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'SZP3LDYMzT9~puEaGTGtdL`0c(}stXDqm?$.]t1@S P-juYjXx7->RvHj 5v L6h' );
+define( 'SECURE_AUTH_KEY',  'PdzI{sp0g-g2ySB/}#Np(-Jo{c6^N| lnLi:6!;o<2=aa$P~)PL.&L3hE`fg={`r' );
+define( 'LOGGED_IN_KEY',    '*nv!W8HC?*0D^H.Yy:W?1]Y04?.<L4*>cuVCS^5Q(3jUm4WXDk3]IwLH.-If*:O_' );
+define( 'NONCE_KEY',        'X;: v.8J9V|i*|DIL*vj/ /LD=&i)Z16CxqaR@4,j)[:0jpYkIE,%`U)IwE9+LJ8' );
+define( 'AUTH_SALT',        'NmKH>WFHU1-W?LM?%*efMBil#e32V;g/<2Bd tixm/EXMf5Q*TV3S)k%*Qd=p&4F' );
+define( 'SECURE_AUTH_SALT', '!=T/}[QA/1E~GUfK_!F(oA$__!%c>KA*fb#n[QRh.h--0~tXLlb)w!)!ApUysCBk' );
+define( 'LOGGED_IN_SALT',   '2z$?la*2Qx}5+9OoGy.E/_s}jP }<.NdH0gG2n6[ fzHx!W;KG^@Mhq3Nla=P{`F' );
+define( 'NONCE_SALT',       '18E@hk}8(@m}m+G8JF8{<34|m8;5FHf6{~V15*ZU=#Uri~Qn,vN>VY^{c2;(JxmZ' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'cpal_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/uploads/custom-css-js/7937.js
@@ -0,0 +1,113 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+ 
+
+jQuery(document).ready(function( $ ){
+  
+  
+  const btnFachada = jQuery('#btnFachada');
+  const btnLobby = jQuery('#btnLobby');
+  const btnPabellon = jQuery('#btnPabellon');
+  
+  const urlRegisterScene = 'https://backend-cenipalma.creategicalatina.com/api/register-scene';
+  const urlRegisterClicks = 'https://backend-cenipalma.creategicalatina.com/api/register-clicks';
+  
+  
+  const dataMarkers = [
+    { nameMarker: "Dar un paso", idMarker: "3266CFA75A", nameScene: "Fachada Inicial", idScene: "7B71545122", points:0 },
+
+    { nameMarker: "Ir al Lobby", idMarker: "FF44A56C97", nameScene: "Fachada después del Un Paso", idScene: "1CB5081B1F", points:0 },
+
+    { nameMarker: "Dar un paso", idMarker: "FEB0FFF06D", nameScene: "Lobby", idScene: "B759528D16", points:0 },
+    
+    { nameMarker: "Ir al Auditorio", idMarker: "720181DEA1", nameScene: "Lobby principal", idScene: "EBF4CA2DCB", points:25 },
+    { nameMarker: "Ir al pabellón", idMarker: "EEB523460E", nameScene: "Lobby principal", idScene: "EBF4CA2DCB", points:25 },
+
+    { nameMarker: "Ir stand 1", idMarker: "1092D575BF", nameScene: "Pabellón", idScene: "632E78A39F", points:100 },
+    { nameMarker: "Ir stand 2", idMarker: "18205CD022", nameScene: "Pabellón", idScene: "632E78A39F", points:100 },
+
+    { nameMarker: "Ir al Lobby", idMarker: "0AFED82CB4", nameScene: "Auditorio", idScene: "632E78A39F", points:0 },
+    { nameMarker: "Ir a la conferencia", idMarker: "404785CC39", nameScene: "Auditorio", idScene: "632E78A39F", points:50 },
+
+    { nameMarker: "Botón de WhatsApp", idMarker: "61CD91CEED", nameScene: "Stand Cenipalma", idScene: "07601BB604", points:200 },
+    { nameMarker: "Botón pagina web", idMarker: "2413A417DD", nameScene: "Stand Cenipalma", idScene: "07601BB604", points:75 },
+    { nameMarker: "Ingresar al Stand", idMarker: "D6154A3C3C", nameScene: "Stand Cenipalma", idScene: "07601BB604", points:0 },
+  ]
+  
+  const dataScenes = [
+    { nameScene: "Fachada Inicial", idScene: "7B71545122" },
+    { nameScene: "Fachada después del Un Paso", idScene: "1CB5081B1F" },
+    { nameScene: "Lobby", idScene: "B759528D16" },
+    { nameScene: "Lobby principal", idScene: "EBF4CA2DCB" },
+    { nameScene: "Pabellón", idScene: "632E78A39F" },
+    { nameScene: "Auditorio", idScene: "A2DEEE00A6" },
+    { nameScene: "Stand Cenipalma", idScene: "07601BB604" },
+    { nameScene: "A dentro de Stan Cenipalma", idScene: "4AF7AE4070" },
+  ]                         
+                       
+  
+  
+  btnFachada.on('click', () => { onSetScene( "7B71545122" ) });
+  btnLobby.on('click', () => { onSetScene( "EBF4CA2DCB" ) });
+  btnPabellon.on('click', () => { onSetScene( "632E78A39F" ) });
+    
+  
+  const onSetScene = ( idScene ) => {
+    window.scene.setScene({ id: idScene });
+    
+    const email = jQuery('#currentUserEmail').val();  
+    const currentScene = dataScenes.find( scene => scene.idScene === idScene );    
+    let dataScene = {};
+    
+    if (idScene === '632E78A39F') {
+       dataScene = { email, go_scene: currentScene.nameScene, nameScene: 'Lobby principal', click_name: 'Ir al pabellón', points: 25 };
+    } else {
+       dataScene = { email, go_scene: currentScene.nameScene, nameScene: '', click_name: '', points: 0 };
+    }   
+
+    registerDataDB( dataScene, urlRegisterScene );
+  }
+  
+      
+  window.onChangeScene = ( e ) => {
+    const idMarker = e.cfg.id;
+    const currentMarker = dataMarkers.find( marker => marker.idMarker === idMarker );
+    
+    if ( currentMarker ) {
+      const idScene = e.cfg.linkSceneId;
+      const email = jQuery('#currentUserEmail').val();  
+      
+      if ( idScene ) { // Go to scene, save info
+        const currentScene = dataScenes.find( scene => scene.idScene === idScene );  
+        const dataScene = { email, go_scene: currentScene.nameScene, nameScene: currentMarker.nameScene, click_name: currentMarker.nameMarker, points: currentMarker.points };
+        
+        registerDataDB( dataScene, urlRegisterScene );
+        
+      } else { // click marker with data        
+        const dataMarker =  { email, nameScene: currentMarker.nameScene, click_name: currentMarker.nameMarker, points: currentMarker.points };
+        
+        registerDataDB( dataMarker, urlRegisterClicks );
+      }      
+    }   
+  }
+  
+  
+  const registerDataDB = ( dataMarker, url ) => { 	
+    
+    if ( dataMarker.email ) {
+     	jQuery.ajax({
+          url: url,
+          type: "POST",
+          data: dataMarker,
+          success: function (response) {
+            console.log('Register... ', response);            
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            console.log('Error... ',textStatus, errorThrown);
+          }
+        }); 
+    }
+  }
+  
+});</script>
+<!-- end Simple Custom CSS and JS -->
